b8d7f40331ac611f0ca4dfd6f620763fb5eda3c7
Rojhat Sinan Balka <sinan@kondukto.io>
2019-10-15 14:29:20 +0300

    Add scan command

---
+++ cmd/scan.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2019 Kondukto
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// scanCmd represents the scan command
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("scan called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+}

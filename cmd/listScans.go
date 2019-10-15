5b7467ed27fa8b5ca9cbd9edf5c3ca4acb79673d
Rojhat Sinan Balka <sinan@kondukto.io>
2019-10-15 11:37:24 +0300

    Remove boilerplate comments

--- cmd/listScans.go
+++ cmd/listScans.go
@@ -60,15 +60,5 @@ func init() {
 	listCmd.AddCommand(listScansCmd)
 
 	listScansCmd.Flags().StringP("project", "p", "", "project name or id")
-	listScansCmd.MarkFlagRequired("project")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// listScansCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// listScansCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	_ = listScansCmd.MarkFlagRequired("project")
 }

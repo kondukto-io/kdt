ef593938c588f86d389f6d0b75b1d0d6b7c0eb32
Rojhat Sinan Balka <sinan@kondukto.io>
2019-10-17 13:31:09 +0300

    Comment out 'Using config file X' log

--- cmd/root.go
+++ cmd/root.go
@@ -85,7 +85,7 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		//fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 
 	if viper.GetString("host") == "" || viper.GetString("token") == "" {

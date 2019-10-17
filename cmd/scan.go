f82e0e1c23e7e2567a2afaa6215db343e2806b77
Rojhat Sinan Balka <sinan@kondukto.io>
2019-10-17 13:09:24 +0300

    Remove scan called log

--- cmd/scan.go
+++ cmd/scan.go
@@ -46,8 +46,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("scan called")
-
 		// Check scan method
 		byScanId := cmd.Flag("scan-id").Changed
 		byProjectAndTool := cmd.Flag("project").Changed &&

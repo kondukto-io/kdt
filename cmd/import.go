fea30326f0754e6d852a5a101eecbcf741b7ff7f
Yusuf <yusuf@endpoint-labs.com>
2021-01-22 18:17:14 +0300

    Reformat import file

--- cmd/import.go
+++ cmd/import.go
@@ -16,49 +16,7 @@ var importCmd = &cobra.Command{
 	Use:   "import",
 	Short: "base command for importing scans",
 	Args:  cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		// Initialize Kondukto client
-		c, err := client.New()
-		if err != nil {
-			qwe(1, err, "could not initialize Kondukto client")
-		}
-
-		// Parse command line flags
-		project, err := cmd.Flags().GetString("project")
-		if err != nil {
-			qwe(1, err, "failed to parse project flag")
-		}
-		branch, err := cmd.Flags().GetString("branch")
-		if err != nil {
-			qwe(1, err, "failed to parse branch flag")
-		}
-		tool, err := cmd.Flags().GetString("tool")
-		if err != nil {
-			qwe(1, err, "failed to parse tool flag")
-		}
-
-		if !validTool(tool) {
-			qwm(1, "invalid tool name")
-		}
-
-		fileList := func() []string {
-			list := make([]string, 0)
-			for _, path := range args {
-				absolutePath, err := filepath.Abs(path)
-				if err != nil {
-					qwe(1, err, "failed to parse absolute path")
-				}
-				list = append(list, absolutePath)
-			}
-			return list
-		}()
-
-		if err := c.ImportScanResult(project, branch, tool, fileList); err != nil {
-			qwe(1, err, "failed to import scan results")
-		}
-
-		qwm(0, "scan results imported")
-	},
+	Run:   importRootCommand,
 }
 
 func init() {
@@ -68,7 +26,51 @@ func init() {
 	importCmd.Flags().StringP("tool", "t", "", "tool name")
 	importCmd.Flags().StringP("branch", "b", "", "branch")
 
-	importCmd.MarkFlagRequired("project")
-	importCmd.MarkFlagRequired("tool")
-	importCmd.MarkFlagRequired("branch")
+	_ = importCmd.MarkFlagRequired("project")
+	_ = importCmd.MarkFlagRequired("tool")
+	_ = importCmd.MarkFlagRequired("branch")
+}
+
+func importRootCommand(cmd *cobra.Command, args []string) {
+	// Initialize Kondukto client
+	c, err := client.New()
+	if err != nil {
+		qwe(1, err, "could not initialize Kondukto client")
+	}
+
+	// Parse command line flags
+	project, err := cmd.Flags().GetString("project")
+	if err != nil {
+		qwe(1, err, "failed to parse project flag")
+	}
+	branch, err := cmd.Flags().GetString("branch")
+	if err != nil {
+		qwe(1, err, "failed to parse branch flag")
+	}
+	tool, err := cmd.Flags().GetString("tool")
+	if err != nil {
+		qwe(1, err, "failed to parse tool flag")
+	}
+
+	if !validTool(tool) {
+		qwm(1, "invalid tool name")
+	}
+
+	fileList := func() []string {
+		list := make([]string, 0)
+		for _, path := range args {
+			absolutePath, err := filepath.Abs(path)
+			if err != nil {
+				qwe(1, err, "failed to parse absolute path")
+			}
+			list = append(list, absolutePath)
+		}
+		return list
+	}()
+
+	if err := c.ImportScanResult(project, branch, tool, fileList); err != nil {
+		qwe(1, err, "failed to import scan results")
+	}
+
+	qwm(0, "scan results imported")
 }

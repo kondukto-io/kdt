4110b68ad1a05d17fc4fcbb8145f91394e3ff7da
Rojhat Sinan Balka <sinan@kondukto.io>
2021-02-04 10:40:36 +0300

    Handle version argument without cobra

--- main.go
+++ main.go
@@ -4,8 +4,19 @@ Copyright © 2019 Kondukto
 */
 package main
 
-import "github.com/kondukto-io/kdt/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/kondukto-io/kdt/cmd"
+)
 
 func main() {
+	args := os.Args
+	if len(args) > 1 && args[1] == "version" {
+		fmt.Println("KDT Kondukto Client v1.0.7")
+		os.Exit(0)
+	}
+
 	cmd.Execute()
 }

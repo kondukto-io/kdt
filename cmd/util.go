a84cea8bcccb403b88a321cf7812928f0cc32454
Yusuf <yusuf@endpoint-labs.com>
2021-06-28 09:23:52 +0300

    Remove unused function and unused variables

--- cmd/util.go
+++ cmd/util.go
@@ -14,31 +14,6 @@ import (
 	"github.com/kondukto-io/kdt/klog"
 )
 
-var scanners = map[string]string{
-	"checkmarx":           "sast",
-	"checkmarxsca":        "sca",
-	"checkmarxkics":       "iac",
-	"owaspzap":            "dast",
-	"webinspect":          "dast",
-	"netsparker":          "dast",
-	"appspider":           "dast",
-	"bandit":              "sast",
-	"findsecbugs":         "sast",
-	"gosec":               "sast",
-	"dependencycheck":     "sca",
-	"fortify":             "sast",
-	"securitycodescan":    "sast",
-	"hclappscan":          "dast",
-	"veracode":            "sast",
-	"burpsuite":           "dast",
-	"burpsuiteenterprise": "dast",
-	"nuclei":              "dast",
-	"gitleaks":            "sast",
-	"semgrep":             "sast",
-	"semgrepconfig":       "iac",
-	"trivy":               "cs",
-}
-
 // qwe quits with error. If there are messages, wraps error with message
 func qwe(code int, err error, messages ...string) {
 	for _, m := range messages {
@@ -52,13 +27,6 @@ func qwe(code int, err error, messages ...string) {
 func qwm(code int, message string) {
 	klog.Println(message)
 	os.Exit(code)
-}
-
-func validTool(t string) bool {
-	if _, ok := scanners[t]; ok {
-		return true
-	}
-	return false
 }
 
 type Row struct {

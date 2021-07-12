f10afaeccec6eec64fe35db6b7c937c7edeecf77
Yusuf <yusuf@endpoint-labs.com>
2021-07-12 12:39:03 +0300

    Add base command create

---
+++ cmd/create.go
@@ -0,0 +1,19 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "base command for create",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			_ = cmd.Help()
+			qwm(0, "")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+}
